b289a0cade8c49c6fcc5d21ef3c8aa938fc7f356
Salinda Hettiarachchi <salinda.hettiarachchi@ebuilder.com>
2016-06-29 11:25:34 +0530

    File Name validation added.

--- LeakHawk/src/com/epsilon/LeakHawk/io/FileManager.java
+++ LeakHawk/src/com/epsilon/LeakHawk/io/FileManager.java
@@ -8,6 +8,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.util.List;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import com.epsilon.Leak.Hawk.model.FeedEntry;
@@ -88,8 +89,7 @@ public class FileManager {
 	}
 	
 	public String getValidFileName( FeedEntry entry ) {
-		
-		Pattern pattern = Pattern.compile("[^/./\\:*?\"<>|]");
+				
 		String fileName = entry.getKey();
 		
 		String entryTitle = entry.getTitle();
@@ -98,11 +98,32 @@ public class FileManager {
 			
 			System.out.println("Entry Title : " + entryTitle );
 			
-			if( !pattern.matcher( entryTitle ).find() ){
+			if( isValidName( entryTitle ) ){
 				fileName = fileName.concat("-").concat( entryTitle );
 			}
 		}		    
 	    return fileName;
+	}
+	
+	public static boolean isValidName(String text) {
+	    Pattern pattern = Pattern.compile(
+	        "# Match a valid Windows filename (unspecified file system).          \n" +
+	        "^                                # Anchor to start of string.        \n" +
+	        "(?!                              # Assert filename is not: CON, PRN, \n" +
+	        "  (?:                            # AUX, NUL, COM1, COM2, COM3, COM4, \n" +
+	        "    CON|PRN|AUX|NUL|             # COM5, COM6, COM7, COM8, COM9,     \n" +
+	        "    COM[1-9]|LPT[1-9]            # LPT1, LPT2, LPT3, LPT4, LPT5,     \n" +
+	        "  )                              # LPT6, LPT7, LPT8, and LPT9...     \n" +
+	        "  (?:\\.[^.]*)?                  # followed by optional extension    \n" +
+	        "  $                              # and end of string                 \n" +
+	        ")                                # End negative lookahead assertion. \n" +
+	        "[^<>:\"/\\\\|?*\\x00-\\x1F]*     # Zero or more valid filename chars.\n" +
+	        "[^<>:\"/\\\\|?*\\x00-\\x1F\\ .]  # Last char is not a space or dot.  \n" +
+	        "$                                # Anchor to end of string.            ", 
+	        Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE | Pattern.COMMENTS);
+	    Matcher matcher = pattern.matcher(text);
+	    boolean isMatch = matcher.matches();
+	    return isMatch;
 	}
 	
 }
